4472614053de1dee13c8b5dbe19fe2929041f4e7
GabrielBorgess <staffgabs@gmail.com>
2024-10-05 13:59:11 -0300

    auth working

---
+++ src/app/auth/signIn/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AuthForm from '@/app/components/AuthForm'
+
+const SignInPage = () => {
+
+  return (
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <div className='flex flex-col'>
+        {/* <h1 className='text-3xl font-bold p-4'>PETICIONADOR</h1> */}
+      </div>
+    <AuthForm />
+    </div>
+  )
+}
+
+export default SignInPage
---
+++ src/app/lib/prisma.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+export default prisma;
+
+async function main() {
+    const hashedPassword = await bcrypt.hash('gabriel', 10)
+  
+    const admin = await prisma.user.upsert({
+      where: { email: 'gabriel@gmail.com' },
+      update: {},
+      create: {
+        name: 'gabriel',
+        email: 'gabriel@gmail.com',
+        passwordHash: hashedPassword,
+      },
+    })
+    console.log({ admin })
+  }
+  
+  main()
+    .then(async () => {
+      await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+      console.error(e)
+      await prisma.$disconnect()
+      process.exit(1)
+    })
