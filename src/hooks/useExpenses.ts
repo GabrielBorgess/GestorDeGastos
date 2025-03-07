26c8c28e52e692d1769ce458865969954e40b7b8
GabrielBorgess <staffgabs@gmail.com>
2025-03-07 19:22:55 -0300

    useExpenses hook

--- src/hooks/useExpenses.ts
+++ src/hooks/useExpenses.ts
@@ -1,5 +1,106 @@
+import { Exepnse } from "@/types/expenses"
+import { useEffect, useState } from "react"
 
-export const useExcenses = () => {
+export const useExcenses = (monthId: string | number) => {
 
+    const [expenses, setExpenses] = useState<Exepnse[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        if (monthId) {
+            console.log("Buscando despesas")
+            fetchExpense();
+        };
+    });
+
+    const fetchExpense = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(`/api/expenses?monthId=${monthId}`);
+            if (!response.ok) {
+                throw new Error('Erro ao buscar despesas');
+            }
+
+            const data = await response.json();
+            setExpenses(data);
+
+
+        } catch (error) {
+            console.error(error);
+            setError('Erro ao buscar despesas');
+        } finally {
+            setLoading(false);
+        };
+    };
+
+    const addExpense = async (descricao: string, valor: number) => {
+        if (!descricao || !valor) {
+            throw new Error('Preencha todos os campos');
+        }
+
+        try {
+            const response = await fetch('/api/expenses', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    monthId: Number(monthId),
+                    description: descricao,
+                    amount: parseFloat(String(valor))
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Erro ao adicionar despesa');
+            };
+
+            const newExpense = await response.json();
+            setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+            return newExpense;
+
+        } catch (error) {
+            console.log(error);
+            setError('Erro ao adicionar despesa');
+            throw error;
+        }
+
+    };
+
+    const deleteExpense = async (despesaId: number) => {
+        try {
+            const response = await fetch(`/api/expenses/?expenseId=${despesaId}`, {
+                method: 'DELETE',
+            });
+
+            if (!response.ok) {
+                throw new Error('Erro ao deletar despesa');
+            };
+
+            setExpenses((prevExpenses) =>
+                prevExpenses.filter((despesa) => despesa.id !== despesaId)
+            );
+
+            return true;
+        } catch (error) {
+            console.error(error);
+            setError('Erro ao deletar despesa');
+            throw error;
+        }
+    }
+
+    const totalExpenses = expenses.reduce(
+        (total, expense) => total + expense.amount,
+        0
+    );
+
+    return {
+        expenses,
+        loading,
+        error,
+        addExpense,
+        deleteExpense,
+        totalExpenses
+    }
 }
- 
